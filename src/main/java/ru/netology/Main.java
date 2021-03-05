e55c2c1319f49ffca736787294f8334694f44a42
Rishat Suleymanov <spk_irbis@mail.ru>
2021-03-05 23:07:45 +0300

    Refactoring

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -1,11 +1,10 @@
 package ru.netology;
 
 public class Main {
-
     private static final int N_WAITERS = 5;
     private static final int N_VISITORS = 5;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Restaurant restaurant = new Restaurant();
         int count = 0;
 
@@ -14,11 +13,7 @@ public class Main {
         do {
             count++;
             new Thread(restaurant::waiterGetOrder, "Waiter " + count).start();
-        } while (count < N_WAITERS);
-        count = 0;
-        do {
-            count++;
             new Thread(restaurant::visitorEntrance, "Visitor " + count).start();
-        } while (count < N_VISITORS);
+        } while (count < N_WAITERS);
     }
 }
--- src/main/java/ru/netology/Restaurant.java
+++ src/main/java/ru/netology/Restaurant.java
@@ -11,21 +11,21 @@ class Restaurant {
     private static final long COOK_MAKES_DISH = 2;
     private static final long VISITOR_MAKES_AN_ORDER = 2;
     private static final long WAITER_DELIVER_THE_ORDER = 2;
-    public static final int EXPECT_VISITORS = 5;
-    List<Dish> dishes;
-    List<Visitor> visitors;
-    Lock locker;
-    Condition conditionWaiter;
-    Condition conditionCook;
-    Condition conditionVisitor;
+    private static final int EXPECT_VISITORS = 5;
+    private final List<Dish> dishes;
+    private final List<Visitor> visitors;
+    private final List<Order> orders;
+    private final Lock locker;
+    private final Condition conditionWaiter;
+    private final Condition conditionCook;
 
     Restaurant() {
+        orders = new ArrayList<>();
         dishes = new ArrayList<>();
         visitors = new ArrayList<>();
         locker = new ReentrantLock();
         conditionWaiter = locker.newCondition();
         conditionCook = locker.newCondition();
-        conditionVisitor = locker.newCondition();
     }
 
     public void cookAtWork() {
@@ -33,12 +33,13 @@ class Restaurant {
         try {
             System.out.println("Cook at work");
             for (int i = 0; i < EXPECT_VISITORS; i++) {
-                while (!dishes.isEmpty()) {
+                while (orders.isEmpty()) {
                     conditionCook.await();
                 }
                 System.out.println("Cook makes dish");
                 TimeUnit.SECONDS.sleep(COOK_MAKES_DISH);
                 dishes.add(new Dish());
+                orders.remove(orders.size() - 1);
                 conditionWaiter.signalAll();
             }
             System.out.println("Cook go home");
@@ -57,13 +58,14 @@ class Restaurant {
                 conditionWaiter.await();
             }
             while (dishes.isEmpty()) {
+                orders.add(new Order());
                 conditionCook.signal();
                 conditionWaiter.await();
             }
 
-            visitors.remove(visitors.size() - 1);
             System.out.println(Thread.currentThread().getName() + " took the order");
             dishes.remove(dishes.size() - 1);
+            visitors.remove(visitors.size() - 1);
             TimeUnit.SECONDS.sleep(WAITER_DELIVER_THE_ORDER);
             conditionWaiter.signal();
         } catch (InterruptedException e) {
